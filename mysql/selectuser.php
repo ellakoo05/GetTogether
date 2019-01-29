d877180d836806a84757756f9f698e5375896eb4
ellakoo05 <ella@github.com>
2019-01-29 11:25:20 -0800

    mysql

--- mysql/selectuser.php
+++ mysql/selectuser.php
@@ -13,19 +13,22 @@ $conn = mysqli_connect($hostname, $username, $password, $dbname, $port);
 if (!$conn) {
 die("Connection failed: " . mysqli_connect_error());
 }
+
+
 else {
 $sql = "INSERT INTO events (eventname, eventdate, eventtime, eventlocation, eventend, eventenddate) VALUES ('{$_POST["eventname"]}','{$_POST["eventdate"]}','{$_POST["eventtime"]}','{$_POST["eventlocation"]}','{$_POST["eventend"]}','{$_POST["eventenddate"]}')";
 }
 
+echo $sql;  
 $res2 = mysqli_query($conn,$sql);
-
+//echo $res2;
 //if($res2 === TRUE){
 //echo "New record created successfully"; 
 //}
 //else{
 //echo "Insert failed"; 
 //};
-
+//
 header("Location:http://localhost:8080/eventpage");
 exit;
 
